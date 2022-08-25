bdcc305d944368a6c745ce528dc2e7d4f4c8148d
Constanza Ortiz <constanza.ortiz@uc.cl>
2022-08-25 17:32:15 -0400

    changes

--- desafios/conyortizsimpson-C.py
+++ desafios/conyortizsimpson-C.py
@@ -2,4 +2,7 @@ a = [67, 111, 110, 115, 116, 97, 110, 122, 97, 32, 79, 114, 116, 105, 122, 32, 8
 lista = []
 for i in a:
     lista.append(chr(i))
-print("".join(lista))
+nombre = lista[0]
+for i in range(1, len(lista)):
+    nombre += lista[i]
+print(nombre)

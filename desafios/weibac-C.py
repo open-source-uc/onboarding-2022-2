aa1d52094ebf07dc9c2218f1d55ade73b6d6adee
weibac <milan.weibel@uc.cl>
2022-08-27 01:34:50 -0400

    weibac-C: shorter

--- desafios/weibac-C.py
+++ desafios/weibac-C.py
@@ -1,8 +1,4 @@
-from codecs import decode
+from codecs import decode  # Mira mama sin chr()
 
 positions = [77, 105, 108, 97, 110, 32, 87, 101, 105, 98, 101, 108]
-bytes = [bytearray(range(a, a + 1)) for a in positions]
-str_out = ''
-for b in bytes:
-    str_out += decode(b)
-print(str_out)
+print(''.join([decode(bytearray(range(a, a + 1))) for a in positions]))

2c24547b264a9e7adf106da27bc821e36bfe4455
Felipe Espinoza <felipe.espinozas@uc.cl>
2022-08-28 23:45:14 -0400

    Readme Modificado

--- desafios/FelipeEspinozaS-A.py
+++ desafios/FelipeEspinozaS-A.py
@@ -5,7 +5,7 @@ def suma(a, b):
         numero_1_nuevo = 0
         while numero_1_nuevo < numero_1:
             numero_1_nuevo += 1
-        
+
         if numero_1_nuevo == numero_1:
             primer_numero = numero_1_nuevo
         else:
@@ -17,15 +17,22 @@ def suma(a, b):
         numero_2_nuevo = 0
         while numero_2_nuevo < numero_2:
             numero_2_nuevo += 1
-        
+
         if numero_2_nuevo == numero_2:
             segundo_numero = numero_2_nuevo
         else:
             segundo_numero = numero_2
 
+    suma_final = 0
 
-    return primer_numero + segundo_numero
+    for i in range(numero_1):
+        for j in range(numero_2):
+            suma_final += 1
+
+    return suma_final
 
 a = int(input('numero 1:'))
 b = int(input('numero 2:'))
 print(suma(a, b))
+
+# Lo siento, se que pudo ser mas largo pero no tuve mucho tiempoe esta semana :c

c18cccb2db1bf374d60c521bc8f5897aeac2c217
catorres8 <catorres8@uc.cl>
2022-08-28 23:58:36 -0400

    aaaaa

---
+++ desafios/catorres8-B.py
@@ -0,0 +1,4 @@
+texto = "catorres8"
+
+texto.capitalize().casefold().center().count().encode().endswith("aaaa").expandtabs().find(
+    "holaaa").format().format_map().index().isalnum().isalpha().isascii().isdecimal().isdigit().isidentifier().islower().isnumeric().isspace().istitle().isupper().join().ljust()
